f5043124289fe8a766ef4c4212a14a0ad74d88eb
LucasDC13 <lucas_de_corte@hotmail.com>
2024-02-02 16:02:41 +0100

    Seed dummy data for Album tables

--- database/migrations/2024_01_30_130959_create_albums_table.php
+++ database/migrations/2024_01_30_130959_create_albums_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('title');
-            $table->timestamp('released');
+            $table->year('released');
             $table->string('artist');
             $table->string('genre');
             $table->string('imgUrl');
